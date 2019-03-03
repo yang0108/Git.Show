a01a041c5fd24f4166a8f51080093241d57ba70f
杨 <874081235@qq.com>
2019-03-03 17:28:54 +0800

    数组

--- GITSHOW/Program.cs
+++ GITSHOW/Program.cs
@@ -9,6 +9,9 @@ namespace GITSHOW
             Console.WriteLine("Hello World!");
             //这是一个注释
             //再加一个注释
+            int[] array = new int[] { 1, 3, 2, 4, 6, 5, 8, 3, 7, 9 };
+            Console.WriteLine("数组1, 3, 2, 4, 6, 5, 8, 3, 7, 9：");
+
         }
     }
 }
